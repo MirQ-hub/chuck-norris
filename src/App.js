72cbb84a15953a99d7c63aaa8203b37e57a225e4
MirQ-hub <n2vami00@students.oamk.fi>
2023-02-27 10:58:52 +0200

    chuck norris

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect, useState} from 'react';
+
+
+const URL = "https://api.chucknorris.io/jokes/random"
 
 function App() {
+  const [title, setTitle] = useState('')
+  const [text, setText] = useState('')
+
+  useEffect(()=>{
+    async function getJoke(){
+      try{
+        const response = await fetch(URL);
+        if (response.ok) {
+          const json = await response.json();
+          const joke = json.contents.chucknorris[0].joke;
+          setTitle(joke.title)
+          setText(joke.text)
+        }
+        else{
+          alert("Sorry, no Chuck Norris today!")
+        }
+      }
+        catch(err){
+          alert(err)
+        }
+        }
+        getJoke()
+      },[])
+    
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: 50}}>
+      <h1>Chuck Norris</h1>
+      <h3>{title}</h3>
+      <p>{text}</p>   
     </div>
+
+
   );
 }
 
